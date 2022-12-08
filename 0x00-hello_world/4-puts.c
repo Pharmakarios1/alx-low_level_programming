9737d64f7dfd904d5c8d8aee916563ac3cc41334
pharmakarios1 <akhigbeblessed@gmail.com>
2022-12-08 10:59:09 -0800

    4 puts task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+
+}
