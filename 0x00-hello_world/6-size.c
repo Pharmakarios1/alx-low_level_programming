1d3735e23d03b2505341293e9ab770dbb3c3b010
pharmakarios1 <akhigbeblessed@gmail.com>
2022-12-08 15:27:37 -0800

    here we go

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * main - Entry
+ *main -is the entry point for the program.
  *
- * Return: Always 0 (Success)
+ *Return:zero if no error, Non-zero value  if errors.
  */
 int main(void)
 {
-	printf("size of char: %lu bytes(s)\n", sizeof(char));
-	printf("size of int: %lu bytes(s)\n", sizeof(int));
-	printf("size of long int: %lu bytes(s)\n", sizeof(long int));
-	printf("size of long long int: %lu bytes(s)\n", sizeof(long long int));
-	printf("size of float: %lu bytes(s)\n", sizeof(float));
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
-
 }
+
