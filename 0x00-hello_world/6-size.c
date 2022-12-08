ab0f113a904d3bbbaa0ded5a78191da3972d62cc
pharmakarios1 <akhigbeblessed@gmail.com>
2022-12-08 14:04:22 -0800

    ubuntu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	printf("size of char: %lu bytes(s)\n", sizeof (char));
-	printf("size of int: %lu bytes(s)\n", sizeof (int));
-	printf("size of long int: %lu bytes(s)\n", sizeof (long int));
-	printf("size of long long int: %lu bytes(s)\n", sizeof (long long int));
-	printf("size of float: %lu bytes(s)\n", sizeof (float));
+	printf("size of char: %lu bytes(s)\n", sizeof(char));
+	printf("size of int: %lu bytes(s)\n", sizeof(int));
+	printf("size of long int: %lu bytes(s)\n", sizeof(long int));
+	printf("size of long long int: %lu bytes(s)\n", sizeof(long long int));
+	printf("size of float: %lu bytes(s)\n", sizeof(float));
 	return (0);
 
 }
