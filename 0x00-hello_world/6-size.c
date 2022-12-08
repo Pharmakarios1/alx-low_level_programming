cf9499e522d96755a7f860485604e2d81ef36420
pharmakarios1 <akhigbeblessed@gmail.com>
2022-12-08 13:47:26 -0800

    sizes of bytes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * return 0 (Success)
+ */
+int main(void)
+{
+	printf("size of char: %lu bytes(s)\n", sizeof (char));
+	printf("size of int: %lu bytes(s)\n", sizeof (int));
+	printf("size of long int: %lu bytes(s)\n", sizeof (long int));
+	printf("size of long long int: %lu bytes(s)\n", sizeof (long long int));
+	printf("size of float: %lu bytes(s)\n", sizeof (float));
+	return (0);
+
+	
+}
