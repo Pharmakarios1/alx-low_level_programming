abc293a81e98d1aae357d1d1bca989f3d1c2d2ba
pharmakarios1 <akhigbeblessed@gmail.com>
2022-12-13 13:24:38 -0800

    print alphabet in times 10 fold

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the a;phabet, in lowercase,
+ * followed by a new line
+ *
+ * this was prepared by Akhigbe Blessed on 13/12/2022
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		alpha = 'a';
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			alpha++;
+		}
+		_putchar('n');
+		i++;
+	}
+}
