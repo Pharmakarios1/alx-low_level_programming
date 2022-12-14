88512f948024cf0a6dc6bd1f90a7c61b62811b37
pharmakarios1 <akhigbeblessed@gmail.com>
2022-12-14 05:52:01 -0800

    Jack bauer

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,13 @@
 #include "main.h"
-
 /**
- * _isalpha -checks for alphabetic character
- * @c: this character to be checked
- *
- * Return: 1 if c is a letter, 0 otherwise
+ * _isalpha - A function that checks if a character is a letter.
+ * @c: parameter of the function
+ * Return: 0 (Always success)
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,23 +7,23 @@
 
 void jack_bauer(void)
 {
-	int i, j;
+	int a, c, d;
 
-	i = 0;
-
-	while (i < 24)
+	for (a = 0; a <= 23; a++)
 	{
-		j = 0;
-		while (j < 60)
+		for (c = 0; c <= 5; c++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-			_putchar(j / 10) + '0');
-			_putchar(j % 10) + '0');
-			_putchar('\n');
-			J++;
+			for (d = 0; d <= 9; d++)
+			{
+				_putchar((a / 10) + '0');
+				_putchar((a % 10) + '0');
+				_putchar(':');
+				_putchar(c + '0');
+				_putchar(d + '0');
+				_putchar('\n');
+			}
+			d = 0;
 		}
-		i++;
+		c = 0;
 	}
 }
