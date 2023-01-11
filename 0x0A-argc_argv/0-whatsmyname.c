5ef2af8b8c69de2b76dd2cd9af8c4b1b7b0c15b5
pharmakarios1 <akhigbeblessed@gmail.com>
2023-01-10 17:32:29 -0800

    1

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - this is a function to print its name
- * @argc: argc param
+ * @argc: argc parameter to count argv elements
  * @argv: an array of a command listed
+ * Exit: (EXIT_SUCCESS)
  * Return: 0 for success
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
-	return (0);
+	(void)argc;
+	printf("%s\n", argv[0]);
+	exit(EXIT_SUCCESS);
 }
